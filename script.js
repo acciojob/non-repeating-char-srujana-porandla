7966e9cd9e19a15e71fa72ff8b9bca1c9f513b59
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-19 11:29:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const charCount={ };
+	for(let i=0;i<str.length;i++){
+		if(charCount[i]){
+			charCount[i]++;
+		}else{
+			charCount=1;
+		}
+	}
+	for(let i=0;i<str.length;i++){
+		if(charCount[i]===1){
+			return i;
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
